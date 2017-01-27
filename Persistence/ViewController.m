95fdaf0aa81f3ed2fa276271361a68d68da3dc04
王慧勇 <weifeng19911024@163.com>
2017-01-27 19:37:59 +0800

    first commit

--- Persistence/ViewController.m
+++ Persistence/ViewController.m
@@ -10,15 +10,46 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *lineFields;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (NSString *)dataFilePath {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    return [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString *filePath = [self dataFilePath];
+    NSLog(@"View Did Load, file at: %@", filePath);
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+        // 读取文件到标签
+        NSArray *array = [[NSArray alloc] initWithContentsOfFile:filePath];
+        for (int i = 0; i < 4; i++) {
+            UITextField *theField = self.lineFields[i];
+            theField.text = array[i];
+        }
+        NSLog(@"display");
+    } else {
+        NSLog(@"file not exists");
+    }
+    // 应用在进入后台前保存数据
+    UIApplication *app = [UIApplication sharedApplication];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:) name:UIApplicationWillResignActiveNotification object:app];
+}
+
+- (void)applicationWillResignActive: (NSNotification *) notification {
+    // 从标签中读取数据到文件
+    NSString *filePath = [self dataFilePath];
+    NSArray *array = [self.lineFields valueForKey:@"text"];	// 迭代
+    [array writeToFile:filePath atomically:YES];
+    NSLog(@"Will Resign Active");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ README.md
@@ -0,0 +1,15 @@
+# Persistence
+
+## 描述
+
+1. 使用属性列表
+
+## 知识点
+
+1. 属性列表
+
+## 参考
+
+《Beginning iPhone Development with Swift: Exploring the iOS SDK》
+
+《精通iOS开发》第7版 第13章 数据持久化
